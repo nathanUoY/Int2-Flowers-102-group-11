76bb89def1d14891b7ac3fa739f287feb5bdbb5f
nathanUoY <126265843+nathanUoY@users.noreply.github.com>
2023-05-04 13:16:28 +0000

    backup for gpu package test

--- flowers.py
+++ flowers.py
@@ -2,11 +2,11 @@
 import tensorflow as tf
 import tensorflow_datasets as tfds
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 #load the dataset with tfds and split into train, validate and test
 flowers, flowers_info = tfds.load('oxford_flowers102', as_supervised= True, with_info= True)
-flowers_train, flowers_valid, flowers_test = flowers['train'], flowers['validation'], flowers['test']
+flowers_train,flowers_valid, flowers_test = flowers['train'], flowers['validation'], flowers['test']
 
 #express the dataset in its splits
 training_length = flowers_info.splits['train'].num_examples
@@ -15,3 +15,16 @@ test_length = flowers_info.splits['test'].num_examples
 print('There are ', training_length, ' images in traning split.')
 print('There are ', validation_length, ' images in validation split.')
 print('There are ', test_length, ' images in test split.')
+
+#print image shape and label for 3 images in training split
+for image, label in flowers_train.take(3):
+    print('\nShape: ', image.shape)
+    print('Label: ', label)
+
+#Plot an image from the training split
+for image, label in flowers_train.take(1):
+    image = image.numpy().squeeze()
+    plt.imshow(image)
+    plt.colorbar()
+    plt.title(label.numpy())
+    plt.show()
